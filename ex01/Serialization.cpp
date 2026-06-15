f5f7a0994528b52b8ab02bf2fd35a17d07d44ade
Imran Barbouch <imranbarbouch@hotmail.fr>
2026-06-15 21:56:34 +0200

    debut 01

---
+++ ex01/Serialization.cpp
@@ -0,0 +1,27 @@
+#include "Data.hpp"
+#include "Serialization.hpp"
+
+Serializer::Serializer() {}
+
+Serializer::Serializer(const Serializer &other)
+{
+	(void)other;
+}
+
+Serializer &Serializer::operator=(const Serializer &other)
+{
+	(void)other;
+	return *this;
+}
+
+Serializer::~Serializer() {}
+
+uintptr_t Serializer::serialize(Data *ptr)
+{
+
+}
+
+Data Serializer::*deserialize(uintptr_t raw)
+{
+
+}
